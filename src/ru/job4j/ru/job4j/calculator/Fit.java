8a1493aa940cc5a9893b3f1e3ab9759f895fbc1b
TatianaLukc <svobody2005@yandex.ru>
2020-10-14 20:12:03 +0300

    ru.job4j.calculator.Fit

--- src/ru/job4j/ru/job4j/calculator/Fit.java
+++ src/ru/job4j/ru/job4j/calculator/Fit.java
@@ -3,11 +3,13 @@ package ru.job4j.ru.job4j.calculator;
 public class Fit {
 
     public static double manWeight(short height) {
-        return (height - 100) * 1.15;
+        double rsl1 = (height - 100) * 1.15;
+        return rsl1;
     }
 
     public static double womanWeight(short height) {
-        return (height - 110) * 1.15;
+        double rsl2 = (height - 110) * 1.15;
+        return rsl2;
     }
 
     public static void main(String[] args) {
